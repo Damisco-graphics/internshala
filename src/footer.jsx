b14ef99bea0a9a5d04bf176c45e1bf52f24d7d10
Damisco-graphics <damilolaadebayo405@gmail.com>
2022-08-02 08:46:00 +0100

    final

--- src/footer.jsx
+++ src/footer.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 
 
 import './body.css'
-import myImage from "./instagram_icon.png"
+import instagram from "./instagram_icon.png"
+import whatsapp from "./whatsapp_icon.png"
+
 
 function footer(){
     return (
     <div id='footer'>
-    <div style={{marginRight:"20%", marginLeft: "2%"}}>
-    <img style={{width:"2rem", height:"2rem"}} src={myImage} alt="instagram icon"/> damisco_creations
-
-    </div>
+    <div style={{marginRight:"5%", marginLeft: "2%"}}>
+    <img style={{width:"2rem", height:"2rem"}} src={instagram} alt="instagram icon"/> damisco_creations
+        </div>
     Copyright© 2022
+    <div style={{marginRight:"2%", marginLeft: "5%"}}>
+    <img style={{width:"2rem", height:"2rem"}} src={whatsapp} alt="instagram icon"/> +2348134274863
+    </div>
     </div>
     )
 }
